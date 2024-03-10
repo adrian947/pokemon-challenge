c85aebe3289277b0fde96ed9cc2ce7c53e2a8040
adrian <aadducchio@leverbox.com>
2024-03-10 17:58:00 -0300

    fix hot reload docker compose

--- Next-pokemon-front/next.config.js
+++ Next-pokemon-front/next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['raw.githubusercontent.com']
-  }
+  },
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
 }
 
 module.exports = nextConfig
